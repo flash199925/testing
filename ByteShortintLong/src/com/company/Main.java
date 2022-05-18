6f4ce92f1ad55506d056abbfb55f181874ad9f32
flash199925 <purohityash99@gmial.com>
2022-05-18 23:21:22 +0530

    testing

---
+++ ByteShortintLong/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int minIntNumber = Integer.MIN_VALUE;
+        System.out.println(minIntNumber);
+        int maxINtNumber = Integer.MAX_VALUE;
+        System.out.println(maxINtNumber);
+    }
+}
